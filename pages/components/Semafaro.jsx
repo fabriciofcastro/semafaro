4cff1d03e370bb1368ae5176fac2c12f62d4157c
Fabricio Castro <fabriciofeitosacastro@gmail.com>
2022-07-21 13:20:52 -0300

    Criado estado de mudança do semaro

---
+++ pages/components/Semafaro.jsx
@@ -0,0 +1,33 @@
+import Buttons from "./buttons";
+import Image from "next/image";
+import imgDesligado from '../../public/image/desligado.png'
+import imgVermelho from '../../public/image/vermelho.png'
+import imgVerde from '../../public/image/verde.png'
+import imgAmarelo from '../../public/image/amarelo.png'
+
+import { useEffect, useState } from "react";
+
+export default function Semafaro(props) {
+
+    const [status, setStatus] = useState(imgDesligado) 
+
+    function validarStatus(e) {
+            corSemafaro[e.target.id]()
+    }
+
+        const corSemafaro = {
+
+            'Vermelho': () => <Image src={setStatus(imgVermelho)} alt="" />,
+            'Amarelo':  () => <Image src={setStatus(imgAmarelo)} alt="" />,
+            'Verde':    () => <Image src={setStatus(imgVerde)} alt="" />,
+            'Desligar': () => <Image src={setStatus(imgDesligado)} alt="" />
+        } 
+
+    return (
+        <div>
+             <Image src={status} alt="" />
+             <Buttons verifica={validarStatus} />
+          
+        </div>
+    )
+}
---
+++ pages/components/buttons.jsx
@@ -0,0 +1,15 @@
+export default function Buttons(props) {
+
+
+
+
+    return (
+
+       <div>
+         <button id="Vermelho" onClick={props.verifica} >Vermeho</button>
+         <button id="Amarelo" onClick={props.verifica} >Amarelo</button>
+         <button id="Verde" onClick={props.verifica} >Verde</button>
+         <button id="Desligar" onClick={props.verifica} >Desligar</button>
+       </div>
+    )
+}
